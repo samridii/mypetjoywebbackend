f1fdfc32cff1d6f8038f8034377c55770495fd73
samridii <samridiic@gmail.com>
2026-01-20 22:17:50 +0545

    Made changes in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
-import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route";
-import connectDB from "./database/mongodb";
-
 dotenv.config();
 
+import express from "express";
+import cors from "cors";
+import connectDatabase from "./database/mongodb";
+import authRoutes from "./routes/auth.route";
+
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`, req.body);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5050;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+async function startServer() {
+  try {
+    await connectDatabase();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+}
+
+startServer();
